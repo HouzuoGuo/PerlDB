0c4b6f594fa124726e33098f2221b775e805a4d4
Houzuo Guo <guohouzuo@gmail.com>
2011-10-02 21:55:47 +1100

    Some simple filter functions to work with RA->select

---
+++ Filters.pm
@@ -0,0 +1,22 @@
+package Filters;
+use strict;
+use warnings;
+use diagnostics;
+use Carp;
+use English qw(-no_match_vars);
+
+# Test if two strings are equal
+sub equals {
+
+    # Parameters: value1, value2
+    my ( $value1, $value2 ) = @_;
+    return $value1 eq $value2;
+}
+
+# Test if one scalar number is less than the another
+sub less_than {
+    # Parameters: value1, value2
+    my ($value1, $value2) = @_;
+    return scalar $value1 < scalar $value2;
+}
+1;
